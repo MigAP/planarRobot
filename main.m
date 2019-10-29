ebbfd64e6a507a03841886f3dd9b5ee43f1f3f08
MigAP <miguel.arpa.p@gmail.com>
2019-10-29 09:03:37 +0100

    organize files

--- main.m
+++ main.m
@@ -20,20 +20,6 @@ global lb = [pointL pointL pointL];
 % Optimization
 H = [13;17]; % Coordinates of the end-point
              % Possible next adjacent configuration 
-%{
-f = @(X) objFunc(X,H,Cf); 
-gFunc = @(X) equal(X,H,l); % equality constraint  
-h = @(X) inequality(X,H,Cf,1); % positive constraint 
-X0 = Cf(1:end-2); % initial feasible configuration 
-
-[y, obj, info, iter, nf, lambda]  = sqp(X0, f,gFunc, [], lb,ub);   
-nbIter = 100; % number of iterations allowed 
-tol = 1e-12; % tolerance of the stopping criteria 
-[x, obj, info, iter]  = sqp(X0, f,gFunc,h, lb,ub,nbIter,tol)    
-
-figure(2);
-printRobot([x;H]); 
-%}
 
 config = calculateConfiguration(Cf, H); 
 %printRobot(config);  
